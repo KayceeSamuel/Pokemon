8dfdcbe7dd78d61d986c3a1a109c1ab6bd5a4714
KayceeSamuel <nwosukelechi90@gmail.com>
2021-06-01 22:56:42 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let pokemon1 = {
+  name:"Bulbasaur",
+  height: 0.7,
+  abilities: ["Chlorophyll", "Overgrow"]
+  };
+
+let pokemon2 = {
+  name: "Kakuna",
+  height: 0.6, 
+  abilities: ["Shed-skin"]
+}; 
+
+let pokemon3 = {
+  name: "Ivysaur",
+  height: 1,
+  abilities: ["Chlorophyll", "Overgrow"]
+  };
+
+let pokemonList =[
+    pokemon1, 
+    pokemon2, 
+    pokemon3
+];
+
+documentwrite(pokemonList.length)
